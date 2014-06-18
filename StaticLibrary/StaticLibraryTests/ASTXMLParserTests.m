175509026fe835bfd2a63eb8bc8791490562be11
Bill Williams <bill@flpatriot.com>
2014-06-18 11:25:15 -0400

    Add tests for ASTXMLParser.

--- StaticLibrary/StaticLibraryTests/ASTXMLParserTests.m
+++ StaticLibrary/StaticLibraryTests/ASTXMLParserTests.m
@@ -1,19 +1,20 @@
 //
-//  StaticLibraryDemoTests.m
-//  StaticLibraryDemoTests
+//  ASTXMLParserTests.m
+//  StaticLibrary
 //
 //  Created by Bill Williams on 18.06.14.
 //  Copyright (c) 2014 A Small Teapot. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+#import "ASTXMLParser.h"
 
 
-@interface StaticLibraryDemoTests : XCTestCase
+@interface ASTXMLParserTests : XCTestCase
 @end
 
 
-@implementation StaticLibraryDemoTests
+@implementation ASTXMLParserTests
 
 - (void)setUp {
     [super setUp];
@@ -24,6 +25,12 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+}
+
+
+- (void)testExample {
+    ASTXMLParser *XMLParser = [[ASTXMLParser alloc] init];
+    XCTAssertNotNil(XMLParser, @"XML Parser should be created from inside test bundle.");
 }
 
 @end
